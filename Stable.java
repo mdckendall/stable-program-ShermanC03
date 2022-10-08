cc2d50fb9d54e2ebe0a756d5bae84f61582eac21
ShermanC03 <111750222+ShermanC03@users.noreply.github.com>
2022-10-07 22:59:54 -0400

    Add files via upload

--- Stable.java
+++ Stable.java
@@ -1,1 +1,11 @@
-//Create the Stable class here.
+import java.util.ArrayList;
+
+
+public class Stable {
+String address = "123 Sesame Street";
+
+  ArrayList<Horse> horseArraylist = new   ArrayList<Horse>();
+  Stable(String address){
+        this.address = address;
+}
+}
