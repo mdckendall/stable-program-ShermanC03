8df1ffdb35bfcc35f758e67e6ab56674221d6582
ShermanC03 <111750222+ShermanC03@users.noreply.github.com>
2022-10-07 23:22:40 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
 public class Main {
-  
   public static void main(String[] args) {
     String userName;
     String address;
     double weight;
     boolean isTame;
 
-    //Ask the user here
 	  Scanner scanner = new Scanner(System.in);
  System.out.println("What is the address of the stable?");
 		address = scanner.nextLine();
@@ -21,18 +19,24 @@ public class Main {
     scanner.nextLine();
     System.out.println("What is the name of the horse");
     userName= scanner.nextLine();
+    
     System.out.println("What is the weight of the horse");
 weight = scanner.nextDouble();
-        System.out.println("Is the horse tamed? Enter True or False");
+    
+  System.out.println("Is the horse tamed? Enter True or False");
 isTame = scanner.nextBoolean();
+    
 Horse horse = new Horse (userName, weight,isTame);
   stable.horseArraylist.add(horse);
      decision= scanner.next().charAt(0);
   }
+    
 			for(int i=0;i<stable.horseArraylist.size();i++){
-        System.out.println("Horse #" + (i+1)+ " Name: "+ stable.horseArraylist.get(i).name + " Weight: "+ stable.horseArraylist.get(i).weight+ " Tame: "+ stable.horseArraylist.get(i).isTame);
-      }
-		
-	  }
-  }
+
+System.out.println("Horse #" + (i+1)+ " Name: "+ stable.horseArraylist.get(i).name + " Weight: "+ stable.horseArraylist.get(i).weight+ " Tame: "+ stable.horseArraylist.get(i).isTame);
+      
+}
+scanner.close();
+ }
+}
 
